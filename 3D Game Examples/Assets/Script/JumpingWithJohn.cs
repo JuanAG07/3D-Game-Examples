ec4e2a6bb60fae1ed8b28da38345285b8fed617c
JuanAG07 <146127030+JuanAG07@users.noreply.github.com>
2024-02-01 12:02:05 -0800

    updated our jumping course

--- 3D Game Examples/Assets/Script/JumpingWithJohn.cs
+++ 3D Game Examples/Assets/Script/JumpingWithJohn.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class JumpingWithJohn : MonoBehaviour
 {
+    public float turnSpeed = 20;
+    public float moveSpeed = 10f;
+    public float JumpForce = 10f;
+    public float gravityModifier =1f;
+    public bool isOnGround = true;
+    private Vector3 _movement;
+    //private Animator m_Animator;
+    private Rigidbody _rigidbody;
+    private Quaternion _rotation = Quaternion.identity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //m_Animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            _rigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        _movement.Set(horizontal, 0f, vertical);
+        _movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, _movement, turnSpeed * Time.deltaTime, 0f);
+        _rotation = Quaternion.LookRotation (desiredForward);
+    
+        _rigidbody.MovePosition (_rigidbody.position + _movement * moveSpeed * Time.deltaTime);
+        _rigidbody.MoveRotation (_rotation);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
     }
 }
